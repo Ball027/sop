19f304a39f183377f33fe718070b0bd42337027a
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-08-15 12:53:57 +0700

    Add files via upload

---
+++ lab4/MathAPI.java
@@ -0,0 +1,36 @@
+package com.example.lab4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MathAPI {
+    @RequestMapping(value = "/plus/{n1}/{n2}", method = RequestMethod.GET)
+    public String myPlus(@PathVariable("n1") double n1, @PathVariable("n2") double n2) {
+        return Double.toString(n1+n2);
+    }
+    @RequestMapping(value = "/minus/{n1}/{n2}", method = RequestMethod.GET)
+    public String myMinus(@PathVariable("n1") double n1, @PathVariable("n2") double n2) {
+        return Double.toString(n1-n2);
+    }
+    @RequestMapping(value = "/multi/{n1}/{n2}", method = RequestMethod.GET)
+    public String myMulti(@PathVariable("n1") double n1, @PathVariable("n2") double n2) {
+        return Double.toString(n1*n2);
+    }
+    @RequestMapping(value = "/divide/{n1}/{n2}", method = RequestMethod.GET)
+    public String myDivide(@PathVariable("n1") double n1, @PathVariable("n2") double n2) {
+        return Double.toString(n1/n2);
+    }
+    @RequestMapping(value = "/mod/{n1}/{n2}", method = RequestMethod.GET)
+    public String myMod(@PathVariable("n1") double n1, @PathVariable("n2") double n2) {
+        return Double.toString(n1%n2);
+    }
+    @RequestMapping(value = "/max", method = RequestMethod.POST)
+    public String myMax(double n1,double n2) {
+        return Double.toString(Math.max(n1,n2));
+    }
+}
