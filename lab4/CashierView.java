9b2e9cf6991911cc7dd0826a9e575812ee9909b6
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-09-04 23:41:58 +0700

    Add files via upload

--- lab4/CashierView.java
+++ lab4/CashierView.java
@@ -3,6 +3,7 @@ package com.example.lab4;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.Route;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,26 +12,26 @@ public class CashierView extends VerticalLayout {
     public CashierView(){
         TextField money = new TextField();
         money.setLabel("เงินทอน");
-        money.setPlaceholder("$");
+        money.setPrefixComponent(new Span("$"));
         Button btn = new Button("คำนวณเงินทอน");
         TextField b1000 = new TextField();
-        b1000.setPlaceholder("$1000:");
+        b1000.setPrefixComponent(new Span("$1000:"));
         TextField b500 = new TextField();
-        b500.setPlaceholder("$500:");
+        b500.setPrefixComponent(new Span("$500:"));
         TextField b100 = new TextField();
-        b100.setPlaceholder("$100:");
+        b100.setPrefixComponent(new Span("$100:"));
         TextField b20 = new TextField();
-        b20.setPlaceholder("$20:");
+        b20.setPrefixComponent(new Span("$20:"));
         TextField b10 = new TextField();
-        b10.setPlaceholder("$10:");
+        b10.setPrefixComponent(new Span("$10:"));
         TextField b5 = new TextField();
-        b5.setPlaceholder("$5:");
+        b5.setPrefixComponent(new Span("$5:"));
         TextField b1 = new TextField();
-        b1.setPlaceholder("$1:");
+        b1.setPrefixComponent(new Span("$1:"));
         add(money,btn,b1000,b500,b100,b20,b10,b5,b1);
 
         btn.addClickListener(Event -> {
-            double cost = Double.parseDouble(money.getValue());
+            int cost = Integer.parseInt(money.getValue());
             Change out = WebClient.create()
                     .get()
                     .uri("http://localhost:8081/getChange/" + cost)
@@ -45,6 +46,7 @@ public class CashierView extends VerticalLayout {
             b10.setValue(String.valueOf(out.getB10()));
             b5.setValue(String.valueOf(out.getB5()));
             b1.setValue(String.valueOf(out.getB1()));
+            System.out.println(out.getB1());
         });
     }
 }
